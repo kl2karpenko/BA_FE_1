5ab0d7d08aaaa656c1a398c30c83e2757e208e0c
l.karpenko <kl2.karpenko@gmail.com>
2016-06-16 23:05:51 +0300

    update 24 HW

--- 21_L_js/example.js
+++ 21_L_js/example.js
@@ -1,7 +1,16 @@
-function printArgs() {
-	// вызов arr.slice() скопирует все элементы из this в новый массив
-	var args = [].slice.call(arguments);
-	alert( args.join(', ') ); // args - полноценный массив из аргументов
+"use strict";
+
+function prost(start, end) {
+	for (var i = start; i <= end; i ++) {
+		for (var j = start; j <= end; j ++) {
+			if (i % j > 0) {
+				console.log(i, i % j, j)
+			}
+		}
+	}
 }
 
-printArgs('Привет', 'мой', 'мир'); // Привет, мой, мир
+prost(2,10)
+
+function Animal(name) {     // this = {};      // в this пишем свойства, методы     this.name = name;     this.canWalk = true;      // return this; }  var elephant = new Animal(‘Elephant');
+	var dog = new Animal('Dog');
